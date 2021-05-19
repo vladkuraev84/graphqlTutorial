29b4a991e6b357202afe4c5629e10567dc5cb369
vladkurev84 <vladkuraev84@gmail.com>
2021-05-19 18:27:03 +0300

    start to add front

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList, GraphQLNonNull } = graphql;
 
 const Movies = require('../models/movie');
 const Directors = require('../models/director');
@@ -28,8 +28,8 @@ const MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: () => ({
         id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: new GraphQLNonNull(GraphQLString) },
         director: {
             type: DirectorType,
             resolve(parent, args) {
@@ -44,8 +44,8 @@ const DirectorType = new GraphQLObjectType({
     name: 'Director',
     fields: () => ({
         id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
         movies: {
             type: new GraphQLList(MovieType),
             resolve(parent, args) {
@@ -54,6 +54,87 @@ const DirectorType = new GraphQLObjectType({
             },
         },
     }),
+});
+
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addDirector: {
+            type: DirectorType,
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) },
+            },
+            resolve(parent, args) {
+                const director = new Directors ({
+                    name: args.name,
+                    age: args.age,
+                });
+                return director.save();
+            }
+        },
+        addMovie: {
+            type: MovieType,
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                genre: { type: new GraphQLNonNull(GraphQLString) },
+                directorId: { type: GraphQLID },
+            },
+            resolve(parent, args) {
+                const movie = new Movies({
+                    name: args.name,
+                    genre: args.genre,
+                    directorId: args.directorId,
+                });
+                return movie.save();
+            }
+        },
+        deleteDirector: {
+            type: DirectorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return Directors.findByIdAndDelete(args.id);
+            }
+        },
+        deleteMovies: {
+            type: MovieType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return Movies.findByIdAndDelete(args.id);
+            }
+        },
+        updateDirector: {
+            type: DirectorType,
+            args: {
+                id: { type: GraphQLID },
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) },
+            },
+            resolve(parent, args) {
+                return Directors.findByIdAndUpdate(
+                  args.id,
+                  { $set: {name: args.name, age: args.age}},
+                  { new: true },
+                )
+            }
+        },
+        updateMovie: {
+            type: MovieType,
+            args: {
+                id: { type: GraphQLID },
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                genre: { type: new GraphQLNonNull(GraphQLString) },
+                directorId: { type: GraphQLID },
+            },
+            resolve(parent, args) {
+                return Movies.findByIdAndUpdate(
+                  args.id,
+                  { $set: {name: args.name, genre: args.genre, directorId: args.directorId} },
+                  { new: true },
+                )
+            }
+        },
+    }
 });
 
 const Query = new GraphQLObjectType({
@@ -90,8 +171,9 @@ const Query = new GraphQLObjectType({
              }
         }
     }
-})
+});
 
 module.exports = new GraphQLSchema({
     query: Query,
-})
+    mutation: Mutation,
+});
--- server/server/app.js
+++ server/server/app.js
@@ -7,7 +7,14 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = 3005;
 
-mongoose.connect('mongodb+srv://vladkuraev84:vlad1984@graphqltutorial.gxmyp.mongodb.net/graphqlTutorialDB?retryWrites=true&w=majority', { dbName: "graphqlTutorialDB", user: "vladkuraev84", pass: "vlad1984", useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://vladkuraev84:vlad1984@graphqltutorial.gxmyp.mongodb.net/graphqlTutorialDB?retryWrites=true&w=majority',
+  {
+    // dbName: "graphqlTutorialDB",
+    // user: "vladkuraev84",
+    // pass: "vlad1984",
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 // mongoose.connect('mongodb+srv://vladkuraev84:vlad1984@graphqltutorial.gxmyp.mongodb.net/graphqlTutorialDB?retryWrites=true&w=majority', { useMongoClient: true });
 
 app.use('/graphql', graphqlHTTP({
